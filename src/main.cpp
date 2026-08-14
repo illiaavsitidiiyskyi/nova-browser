c0188d6efd1c14e90f4920c12365ac979bf9d132
Avsitidiiskyi <avsitidiiskyi@ilkali.2378.129b.4f5e.ip.kyivstar.net>
2026-08-14 11:30:52 +0200

    feat: initialize project skeleton

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Nova Browser core: starting up..." << std::endl;
+    // TODO: CEF initialization goes here (Stage 2)
+    return 0;
+}
